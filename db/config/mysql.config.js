8c09695bbae0e60fda261838605b2a960707cfce
Fede <fedenegreira@gmail.com>
2024-07-06 16:57:20 -0300

    modif

--- db/config/mysql.config.js
+++ db/config/mysql.config.js
@@ -2,7 +2,7 @@ const config = {
     host: 'sql3.freesqldatabase.com',
     user: 'sql3718199',
     password: 'eVe44Hq8lx',
-    database: 'shakira_app'
+    database: 'sql3718199'
 }
 
 export default config;
